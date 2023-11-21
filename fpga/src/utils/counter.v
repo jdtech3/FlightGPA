405892c14e2499a947047936c140e63e3224f60f
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2023-11-21 16:50:31 -0500

    changed some resets to active high

--- fpga/src/utils/counter.v
+++ fpga/src/utils/counter.v
@@ -1,9 +1,9 @@
-module counter(clock, resetn, enable, out);
+module counter(clock, reset, enable, out);
 	parameter WIDTH = 8;
-	input clock, resetn, enable;
+	input clock, reset, enable;
 	output reg [WIDTH-1:0] out;
 	always @(posedge clock) begin
-		if(!resetn) out <= 0;
+		if(reset) out <= 0;
 		else if(enable) out <= out + 1;
 	end
 endmodule
--- fpga/src/utils/grid_counter.v
+++ fpga/src/utils/grid_counter.v
@@ -1,6 +1,6 @@
-module grid_counter(clock, resetn, enable, x_max, y_max, x, y, end_of_grid);
+module grid_counter(clock, reset, enable, x_max, y_max, x, y, end_of_grid);
 	parameter WIDTH=8;
-	input wire clock, resetn, enable;
+	input wire clock, reset, enable;
 	input wire [WIDTH-1:0] x_max, y_max;
 	output wire [WIDTH-1:0] x, y;
 	output end_of_grid;
@@ -10,6 +10,6 @@ module grid_counter(clock, resetn, enable, x_max, y_max, x, y, end_of_grid);
 	assign y_max_reached = y == y_max;
 	assign end_of_grid = x_max_reached & y_max_reached;
 	
-	counter #(WIDTH) xCounter(clock, resetn & ~x_max_reached, enable, x);
-	counter #(WIDTH) yCounter(clock, resetn & ~end_of_grid, x_max_reached, y);
+	counter #(WIDTH) xCounter(clock, reset | x_max_reached, enable, x);
+	counter #(WIDTH) yCounter(clock, reset | end_of_grid, x_max_reached, y);
 endmodule
