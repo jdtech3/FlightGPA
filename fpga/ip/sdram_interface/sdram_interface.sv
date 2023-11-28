a10c30a0d196a77dda8e099ab544cd0b057c400c
Joe Dai <dm.joedai@gmail.com>
2023-11-28 15:57:05 -0500

    FPGA: more work on SDRAM interface state machines

--- fpga/ip/sdram_interface/sdram_interface.sv
+++ fpga/ip/sdram_interface/sdram_interface.sv
@@ -14,6 +14,7 @@
         - Does not implement bursting as SDRAM controller IP does not seem to support bursting.
         - Does not check bounds
         - Assumes 32-bit address width
+        - 24-bit data width does NOT work due to Avalon adapter issues
 
 */
 
@@ -30,7 +31,7 @@ module sdram_interface #(
     output logic                    wm_write,
     input logic                     wm_waitrequest,
 
-    // Avalon master I/O for reader
+    // Avalon master I/O for reader 
     output logic [31:0]             rm_address,
     input logic [DATA_WIDTH-1:0]    rm_readdata,
     output logic                    rm_read,
@@ -58,10 +59,11 @@ module sdram_interface #(
     typedef enum bit [3:0] {
         MODULE_IDLE,
         MODULE_START,
-        MODULE_FETCH,
+        MODULE_FETCHING,
+        MODULE_FETCH_DONE,
         MODULE_WAIT,
-        MODULE_FLUSH,
-        MODULE_DONE
+        MODULE_FLUSHING,
+        MODULE_FLUSH_DONE
     } module_state_t;
 
     typedef enum bit [3:0] { 
@@ -84,15 +86,58 @@ module sdram_interface #(
     
     // --- Signals ---
 
-    reg write_go;
-    wire write_done;
-    reg write_write;
-    wire write_buffer_full;
-    sdram_writer_state_t write_current_state;
-    sdram_writer_state_t write_next_state;
-    
+    // Writer
+    logic writer_go;
+    logic writer_done;
+    logic writer_write;
+    logic writer_buffer_full;
+    sdram_writer_state_t writer_current_state;
+    sdram_writer_state_t writer_next_state;
+
+    // Reader
+    logic reader_go;
+    logic reader_done;
+    logic reader_read;
+    logic reader_data_available;
+    logic [DATA_WIDTH-1:0] reader_data;
+    sdram_writer_state_t reader_current_state;
+    sdram_writer_state_t reader_next_state;
+
+    // Color counter (testing)
     reg [23:0] color_counter;
 
+    // --- Logic ---
+
+    always_comb begin
+        case (writer_current_state)
+            SDRAM_WRITER_IDLE:          writer_next_state = SDRAM_WRITER_START;
+            SDRAM_WRITER_START:         writer_next_state = SDRAM_WRITER_PRETRANSFER;
+            SDRAM_WRITER_PRETRANSFER:   writer_next_state = SDRAM_WRITER_WRITING;
+            SDRAM_WRITER_WRITING:       writer_next_state = SDRAM_WRITER_POST_WRITE;
+            SDRAM_WRITER_POST_WRITE:    writer_next_state = writer_buffer_full ? SDRAM_WRITER_POST_WRITE :   	// also stuck here if FIFO full
+                                                                                 (writer_done ? SDRAM_WRITER_DONE : SDRAM_WRITER_WRITING);
+            SDRAM_WRITER_DONE:          writer_next_state = SDRAM_WRITER_DONE;
+            default:                    writer_next_state = SDRAM_WRITER_IDLE;
+        endcase
+    end
+    
+    always_ff @ (posedge clk) begin
+        if (reset) begin
+            writer_current_state <= SDRAM_WRITER_IDLE;
+            color_counter <= 0;
+        end
+        else writer_current_state <= writer_next_state;
+        
+        if (writer_current_state == SDRAM_WRITER_WRITING) color_counter <= color_counter + 54;
+    end
+    
+    always_comb begin
+        writer_go = writer_current_state == SDRAM_WRITER_START;
+        writer_write = writer_current_state == SDRAM_WRITER_WRITING;
+    end
+    
+    // --- Modules ---
+    
     write_master #(
         .DATAWIDTH(DATA_WIDTH),
 		.FIFODEPTH(FIFO_DEPTH),
@@ -103,14 +148,14 @@ module sdram_interface #(
         
         .control_fixed_location(1'b0),
         .control_write_base(32'h00000000),
-        .control_write_length(32'd1228800),
-        .control_go(write_go),
-        .control_done(write_done),
+        .control_write_length(32'd1228800),      // in bytes
+        .control_go(writer_go),
+        .control_done(writer_done),
         
 //		.user_buffer_data(color),
         .user_buffer_data({8'hFF, color_counter[23:0]}),
-        .user_write_buffer(write_write),
-        .user_buffer_full(write_buffer_full),
+        .user_write_buffer(writer_write),
+        .user_buffer_full(writer_buffer_full),
         
         .master_address(wm_address),
         .master_write(wm_write),
@@ -125,43 +170,25 @@ module sdram_interface #(
         .MAXBURSTCOUNT(1),      // we don't use bursting
         .BURSTCOUNTWIDTH(1)
 	) reader (
-		// Connections
 		.clk(clk),
 		.reset(reset),
-		.master_address(avm_m0_address),
-		.master_read(avm_m0_read),
-		.master_byteenable(avm_m0_byteenable),
-		.master_readdata(avm_m0_readdata),
-		.master_readdatavalid(avm_m0_readdatavalid),
-		.master_burstcount(avm_m0_burstcount),
-		.master_waitrequest(avm_m0_waitrequest)
+
+        .control_fixed_location(1'b0),
+        .control_read_base(32'h00000000),
+        .control_read_length(32'd921600),       // in bytes
+        .control_go(1'b0),
+        // .control_go(reader_go),
+        .control_done(reader_done),
+
+        .user_buffer_data(reader_data),
+        .user_read_buffer(reader_read),
+        .user_data_available(reader_data_available),
+
+		.master_address(rm_address),
+		.master_read(rm_read),
+		.master_readdata(rm_readdata),
+		.master_readdatavalid(rm_readdatavalid),
+		.master_waitrequest(rm_waitrequest)
 	);
     
-    always_comb begin
-        case (write_current_state)
-            SDRAM_WRITER_IDLE:          write_next_state = SDRAM_WRITER_START;
-            SDRAM_WRITER_START:         write_next_state = SDRAM_WRITER_PRETRANSFER;
-            SDRAM_WRITER_PRETRANSFER:   write_next_state = SDRAM_WRITER_WRITING;
-            SDRAM_WRITER_WRITING:       write_next_state = SDRAM_WRITER_POST_WRITE;
-            SDRAM_WRITER_POST_WRITE:    write_next_state = write_buffer_full ? SDRAM_WRITER_POST_WRITE :   	// also stuck here if FIFO full
-                                                                               (write_done ? SDRAM_WRITER_DONE : SDRAM_WRITER_WRITING);
-            SDRAM_WRITER_DONE:          write_next_state = SDRAM_WRITER_DONE;
-            default:                    write_next_state = SDRAM_WRITER_IDLE;
-        endcase
-    end
-    
-    always_ff @ (posedge clk) begin
-        if (reset) begin
-            write_current_state <= SDRAM_WRITER_IDLE;
-            color_counter <= 0;
-        end
-        else write_current_state <= write_next_state;
-        
-        if (write_current_state == SDRAM_WRITER_WRITING) color_counter <= color_counter + 54;
-    end
-    
-    always_comb begin
-        write_go = write_current_state == SDRAM_WRITER_START;
-        write_write = write_current_state == SDRAM_WRITER_WRITING;
-    end
 endmodule
